744f67bc956c2a75f23e281365bdd1fbe680d808
willemtijm <willemtijm@gmail.com>
2022-07-08 16:39:11 +0200

    Model  bijgewerkt

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -10,3 +10,20 @@ class Topic(models.Model):
     def __str__(self):
         """Return a string representation of the model."""
         return self.text
+
+class Entry(models.Model):
+    """Something specific learned about a topic"""
+    topic = models.ForeignKey(Topic, on_delete=models.DO_NOTHING)
+    #topic = models.CharField(max_length=200)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        if len(self.text) > 50:
+            return self.text[:50] + "..."
+        else:
+            return self.text
